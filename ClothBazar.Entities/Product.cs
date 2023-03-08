6fd4dc8bc7dd382d637bc93ab6b015c456dbbba8
Muhammad-aqib2020 <75481667+Muhammad-aqib2020@users.noreply.github.com>
2023-03-08 16:49:35 +0500

    Done

--- ClothBazar.Entities/Product.cs
+++ ClothBazar.Entities/Product.cs
@@ -9,6 +9,7 @@ namespace ClothBazar.Entities
     public class Product : BaseEntity
     {
         public decimal Price { get; set; }
-        public Category Category { get; set; }
+      //  public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
